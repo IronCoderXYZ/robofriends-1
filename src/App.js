bc88ff21a6bf3e89133fc35b87456483e8d791bb
Leon <leon.karho.chung@gmail.com>
2018-01-25 18:03:20 -0500

    add css, create components

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Cardlist from './Cardlist';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const filteredRobot = this.state.robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+                <h1 className='f2'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Cardlist robots={filteredRobot}/>
+            </div>
+        );
+    };
+};
+
+export default App;
