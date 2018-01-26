291e2596aa1b604e5a984fb1d0f0b8cb9a29926f
Leon <leon.karho.chung@gmail.com>
2018-01-26 17:01:07 -0500

    create Scroll component wrap around Cardlist

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import Cardlist from './Cardlist';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => this.setState({ robots : users }))
     }
 
     onSearchChange = (event) => {
@@ -21,13 +27,19 @@ class App extends Component {
         const filteredRobot = this.state.robots.filter((robot) => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className='tc'>
-                <h1 className='f2'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Cardlist robots={filteredRobot}/>
-            </div>
-        );
+        if(this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f2'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <Cardlist robots={filteredRobot}/>
+                    </Scroll>
+                </div>
+            );
+        }
     };
 };
 
